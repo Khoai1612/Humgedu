6429029178d2a79370b3902204bdd5e33d8fc824
NGUYEN THI THANH HANG <1721050227@student.humg.edu.vn>
2021-08-24 14:45:57 +0700

    1

--- Bai2/Bai2/Controllers/DemoController.cs
+++ Bai2/Bai2/Controllers/DemoController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Bai2.Models;
 
 namespace Bai2.Controllers
 {
     public class DemoController : Controller
     {
+        GiaiPT gpt = new GiaiPT();
+
         // GET: Demo
         public ActionResult Index()
         {
@@ -15,6 +18,17 @@ namespace Bai2.Controllers
         }
         public ActionResult Hello()
         {
+            return View();
+        }
+        public ActionResult GiaiPTB1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GiaiPTB1(double hesoA, double hesoB)
+        {
+            double x = gpt.GiaiPhuongTrinhBacNhat(hesoA, hesoB);
+            ViewBag.nghiemPT = x;
             return View();
         }
     }
---
+++ Bai2/Bai2/Models/GiaiPT.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bai2.Models
+{
+    public class GiaiPT
+    {
+        public double GiaiPhuongTrinhBacNhat(double a,double b)
+        {
+            // ax+b=0
+            double x;
+            x = -b / a;
+            return x;
+        }
+    }
+}
