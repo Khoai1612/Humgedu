bf541b74ace1faba2694e78cfeabbebe28ccd8df
NGUYEN THI THANH HANG <1721050227@student.humg.edu.vn>
2021-08-24 14:00:47 +0700

    1

---
+++ Bai2/Bai2/Controllers/DemoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai2.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hello()
+        {
+            return View();
+        }
+    }
+}
