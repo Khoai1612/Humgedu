db1dda8a774ef8c9a03b533df3c09fec75939a37
NGUYEN THI THANH HANG <1721050227@student.humg.edu.vn>
2021-08-27 13:07:10 +0700

    1

---
+++ Bai2/Bai2/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai2.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetInfo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GetInfo (String ten,string tuoi,string lop)
+        {
+            ViewBag.ten = ten;
+            ViewBag.tuoi = tuoi;
+            ViewBag.lop = lop;
+            return View();
+        }
+            
+    }
+}
