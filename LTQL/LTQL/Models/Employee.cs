173fda69affeb6e236f037e964e220897f8cd123
NGUYEN THI THANH HANG <1721050227@student.humg.edu.vn>
2021-09-28 14:53:11 +0700

    1

--- LTQL/LTQL/Models/Employee.cs
+++ LTQL/LTQL/Models/Employee.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace LTQL.Models
 {
     public class Employee : People
     {
+        [AllowHtml]
         public string Company { get; set; }
         public string Address { get; set; }
     }
